67c306f4ac97c7d65ee74ed4ca63f3031e016f1f
Umersaleem98 <umer.saleem.abbasi109@gmail.com>
2025-01-20 16:06:56 +0500

    footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,68 +1,68 @@
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-4 col-md-6">
-                    <h5 class="text-white mb-4">Address</h5>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>
-                        19 St Peter’s close London IG2 7QL</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>07973755842</p>
-                    {{-- <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>info@ukmarketingservices.co.uk</p> --}}
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@ukmarketingservices.co.uk</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <h5 class="text-white mb-4">Quick Links</h5>
-                    {{-- <a class="btn btn-link" href="{{ url('') }}">Contact</a> --}}
-                    <a class="btn btn-link" href="{{ url('about-us') }}">About Us</a>
-                    <a class="btn btn-link" href="{{ url('our-services') }}">Services</a>
-                    <a class="btn btn-link" href="{{ url('partners') }}">Partners</a>
-                    <a class="btn btn-link" href="{{ url('suppliers') }}">Supplier</a>
-                    <a class="btn btn-link" href="{{ url('contact-us') }}">Contact</a>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <h5 class="text-white mb-4">Project Gallery</h5>
-                    <div class="row g-2">
-                        <div class="col-4">
-                            <img class="img-fluid rounded" src="home/img/gallery-1.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid rounded" src="home/img/gallery-2.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid rounded" src="home/img/gallery-3.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid rounded" src="home/img/gallery-4.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid rounded" src="home/img/gallery-5.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid rounded" src="home/img/gallery-6.jpg" alt="">
+        <!-- Footer Start -->
+        <div class="container-fluid bg-dark text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5">
+                <div class="row g-5">
+                    <div class="col-lg-4 col-md-6">
+                        <h5 class="text-white mb-4">Address</h5>
+                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>
+                            19 St Peter’s close London IG2 7QL</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>07973755842</p>
+                        {{-- <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>info@ukmarketingservices.co.uk</p> --}}
+                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@ukmarketingservices.co.uk</p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
-                </div>
-               
-            </div>
-        </div> 
-        <div class="container">
-            <div class="copyright">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        {{-- &copy; <a href="#">Your Site Name</a>, All Right Reserved. --}}
+                    <div class="col-lg-4 col-md-6">
+                        <h5 class="text-white mb-4">Quick Links</h5>
+                        {{-- <a class="btn btn-link" href="{{ url('') }}">Contact</a> --}}
+                        <a class="btn btn-link" href="{{ url('about-us') }}">About Us</a>
+                        <a class="btn btn-link" href="{{ url('our-services') }}">Services</a>
+                        <a class="btn btn-link" href="{{ url('partners') }}">Partners</a>
+                        <a class="btn btn-link" href="{{ url('suppliers') }}">Supplier</a>
+                        <a class="btn btn-link" href="{{ url('contact-us') }}">Contact</a>
                     </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        {{-- Designed By <a href="https://htmlcodex.com">HTML Codex</a>
-                        <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a> --}}
+                    <div class="col-lg-4 col-md-6">
+                        <h5 class="text-white mb-4">Project Gallery</h5>
+                        <div class="row g-2">
+                            <div class="col-4">
+                                <img class="img-fluid rounded" src="home/img/images.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded" src="home/img/about.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded" src="home/img/all_energy_solar_cover.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded" src="home/img/all_energy_solar_cover1.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded" src="home/img/all_energy_solar_cover2.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded" src="home/img/all_energy_solar_cover3.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
+            </div> 
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            {{-- &copy; <a href="#">Your Site Name</a>, All Right Reserved. --}}
+                        </div>
+                        <div class="col-md-6 text-center text-md-end">
+                            {{-- Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                            <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a> --}}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- Footer End -->
+        <!-- Footer End -->
